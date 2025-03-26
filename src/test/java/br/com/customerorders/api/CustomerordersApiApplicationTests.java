d29c8f8d755c51b66562a690be31fd34c7dbafb5
Otávio Brisolla <otavio.polatto@gmail.com>
2025-03-26 19:43:33 -0300

    teste exemplo

--- src/test/java/br/com/customerorders/api/CustomerordersApiApplicationTests.java
+++ src/test/java/br/com/customerorders/api/CustomerordersApiApplicationTests.java
@@ -1,13 +1,29 @@
 package br.com.customerorders.api;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.customerorders.api.domain.entity.Store;
+import br.com.customerorders.api.repository.StoreRepository;
 
 @SpringBootTest
 class CustomerordersApiApplicationTests {
+	
+	@Autowired
+	StoreRepository storeRepository;
 
 	@Test
 	void contextLoads() {
+		
+		List<Store> storeList = storeRepository.findAll();
+		
+		Assertions.assertTrue(storeList.size() > 0);
+		
+		storeList.forEach(System.out::println);
 	}
 
 }
